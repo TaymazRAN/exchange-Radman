60ffc015bbc557ef0a2806cdaba63a65278f6b99
NIBBA <m.sinakheiry2@gmail.com>
2023-01-01 20:51:19 +0330

    Header And Project Map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
+import "./App.scss";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Landing from "./pages/landing/Landing";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Landing />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,39 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+	return (
+		<div className="header">
+			<div className="widthFix">
+				<Link to="/" className="logoBox">
+					<span className="name">کارگزاری صبا جهاد</span>
+					<div className="logo"></div>
+				</Link>
+				<div className="buttonBox">
+					<Button className="button" variant="text" color="error">
+						درباره ما
+					</Button>
+					<Button className="button" variant="text" color="error">
+						خدمات
+					</Button>
+					<Button className="button" variant="text" color="error">
+						سامانه های معاملاتی
+					</Button>
+					<Button className="button" variant="text" color="error">
+						تماس با ما
+					</Button>
+				</div>
+				<div className="buttonBox">
+					<Button
+						className="button contained"
+						variant="text"
+						color="error"
+					>
+						ورود / ثبت نام
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
