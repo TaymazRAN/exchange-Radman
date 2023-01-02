6bf2ac7ffe0ecff03a203bc947457cebb38c17aa
Taymaz Ranjbari <89608125+TaymazRAN@users.noreply.github.com>
2023-01-02 23:23:36 +0330

    change logo ...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,32 +4,32 @@ import { Link } from "react-router-dom";
 import SuperMenu from "../superMenu/SuperMenu";
 
 export default function Header() {
-	return (
-		<div className="header">
-			<div className="widthFix">
-				<Link to="/" className="logoBox">
-					<div className="logo"></div>
-					<span className="name">کارگزاری صبا جهاد</span>
-				</Link>
-				<div className="buttonBox">
-					<Button className="button" variant="text" color="error">
-						درباره ما
-					</Button>
-					<SuperMenu />
-					<Button className="button" variant="text" color="error">
-						سامانه های معاملاتی
-					</Button>
-					<Button className="button" variant="text" color="error">
-						تماس با ما
-					</Button>
-				</div>
+  return (
+    <div className="header">
+      <div className="widthFix">
+        <Link to="/" className="logoBox">
+          <div className="logo"></div>
+          {/* <span className="name">کارگزاری صبا جهاد</span> */}
+        </Link>
+        <div className="buttonBox">
+          <Button className="button" variant="text" color="error">
+            درباره ما
+          </Button>
+          <SuperMenu />
+          <Button className="button" variant="text" color="error">
+            سامانه های معاملاتی
+          </Button>
+          <Button className="button" variant="text" color="error">
+            تماس با ما
+          </Button>
+        </div>
 
-				<div className="buttonBox">
-					<Button className="button contained" variant="text" color="error">
-						ورود / ثبت نام
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="buttonBox">
+          <Button className="button contained" variant="text" color="error">
+            افتتاح حساب
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
