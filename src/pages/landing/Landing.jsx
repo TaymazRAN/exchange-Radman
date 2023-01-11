56866436f7986f586e5a0bbc4c27aaf38a37cbf5
Taymaz Ranjbari <89608125+TaymazRAN@users.noreply.github.com>
2023-01-11 06:48:03 +0330

    change landing

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Faq from '../../components/faq/Faq';
-import Slider from '../../components/slider/Slider';
+import React from "react";
+import Faq from "../../components/faq/Faq";
+import Slider from "../../components/slider/Slider";
+import Branch from "./branch/Branch";
 
 const Landing = () => {
   return (
-		<div>
-			<div className="widthFix">
+    <div>
+      <div className="widthFix">
         <Slider />
         <Faq />
-			</div>
-		</div>
-	);
-}
+        <Branch />
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
