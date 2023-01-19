cb2057c441cb3e1eddf9e333019626ffc1a821d7
NIBBA <m.sinakheiry2@gmail.com>
2023-01-19 18:44:38 +0330

    Branches Done, Contact Done

--- src/App.js
+++ src/App.js
@@ -7,21 +7,40 @@ import Login from "./pages/landing/account/Login";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import Static from "./pages/static/Static";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import rtlPlugin from "stylis-plugin-rtl";
+
+const theme = createTheme({
+	direction: "rtl",
+});
+
+const cacheRtl = createCache({
+	key: "muirtl",
+	stylisPlugins: [prefixer, rtlPlugin],
+});
 
 function App() {
 	return (
-		<div className="App">
-			<Provider store={store}>
-				<Router>
-					<Routes>
-						<Route path="/" element={<Landing />} />
-						<Route path="/static/*" element={<Static />} />
-						<Route path="/login/*" element={<Login />} />
-						<Route path="/admin/*" element={<PanelAdmin />} />
-					</Routes>
-				</Router>
-			</Provider>
-		</div>
+		<CacheProvider value={cacheRtl}>
+			<ThemeProvider theme={theme}>
+				<div className="App">
+					<Provider store={store}>
+						<Router>
+							<Routes>
+								<Route path="/" element={<Landing />} />
+								<Route path="/static/*" element={<Static />} />
+								<Route path="/login/*" element={<Login />} />
+								<Route path="/admin/*" element={<PanelAdmin />} />
+							</Routes>
+						</Router>
+					</Provider>
+				</div>
+			</ThemeProvider>
+		</CacheProvider>
 	);
 }
 
--- src/pages/static/contact/Branch.jsx
+++ src/pages/static/contact/Branch.jsx
@@ -7,56 +7,254 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Branch = () => {
   return (
-    <div className="page">
-      <h2>شعب و دفاتر</h2>
-      <div className="accordionContainer">
-        <Accordion className="customAccordion">
-          <AccordionSummary
-            expandIcon={
-              <ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
-            }
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <div className="customQuestion">
-              شعبه تهران (تالار بزرگمهر)
-              <div className="customBorder"></div>
-            </div>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              <div className="customAnswer">
-                <ul>
-                  <li>
-                    <b>نام مدیر:</b>آقای ابوالفضل ابراهیم زاده
-                  </li>
-                  <li>
-                    <b>نشانی:</b>خیابان ولیعصر، خیابان بزرگمهر، بعد از تقاطع
-                    فلسطین، پلاک 38
-                  </li>
-                  <li>
-                    <b>تلفن:</b>۶۳۴۸۶۰۰ - ۰۲۱
-                  </li>
-                  <li>
-                    <b>فکس:</b>۶۳۴۸۶۰۰ - ۰۲۱
-                  </li>
-                  <li>
-                    <b>مسئول شعبه:</b>علی محمدی
-                  </li>
-                  <li>
-                    <b>معامله‌گر:</b>محمد علی‌زاده
-                  </li>
-                  <li>
-                    <b>پذیرش:</b>فاطمه بیات
-                  </li>
-                </ul>
-              </div>
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </div>
-    </div>
-  );
+		<div className="page">
+			<h2>شعب و دفاتر</h2>
+			<div className="accordionContainer">
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه تهران (تالار بزرگمهر)
+							<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>نام مدیر:</b>آقای ابوالفضل ابراهیم زاده
+									</li>
+									<li>
+										<b>نشانی:</b>خیابان ولیعصر، خیابان بزرگمهر، بعد از تقاطع
+										فلسطین، پلاک 38
+									</li>
+									<li>
+										<b>تلفن:</b>۶۳۴۸۶۰۰ - ۰۲۱
+									</li>
+									<li>
+										<b>فکس:</b>۶۳۴۸۶۰۰ - ۰۲۱
+									</li>
+									<li>
+										<b>مسئول شعبه:</b>علی محمدی
+									</li>
+									<li>
+										<b>معامله‌گر:</b>محمد علی‌زاده
+									</li>
+									<li>
+										<b>پذیرش:</b>فاطمه بیات
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه تهران (تالار سعادت‌آباد)
+							<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه تبریز
+							<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه کاشان
+							<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه نوشهر
+							<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه نیشابور
+							<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه ساری 1<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه ساری 2<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+				<Accordion className="customAccordion">
+					<AccordionSummary
+						expandIcon={
+							<ExpandMoreIcon style={{ color: "#D0CD38", fontSize: 25 }} />
+						}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+					>
+						<div className="customQuestion">
+							شعبه شیراز
+							<div className="customBorder"></div>
+						</div>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							<div className="customAnswer">
+								<ul>
+									<li>
+										<b>تکست:</b>تکست
+									</li>
+								</ul>
+							</div>
+						</Typography>
+					</AccordionDetails>
+				</Accordion>
+			</div>
+		</div>
+	);
 };
 
 export default Branch;
