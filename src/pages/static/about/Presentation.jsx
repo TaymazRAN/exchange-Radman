61d8cb9e702f4bf04073d454038e874176a3305f
NIBBA <m.sinakheiry2@gmail.com>
2023-01-31 16:35:05 +0330

    About Page Done

--- src/pages/static/about/Presentation.jsx
+++ src/pages/static/about/Presentation.jsx
@@ -1,32 +1,148 @@
 import React from "react";
-import Accordion from "@mui/material/Accordion";
-import Typography from "@mui/material/Typography";
 
 const Presentation = () => {
-  return (
-    <div className="page">
-      <h2> معرفی صبا جهاد</h2>
-      <div className="accordionContainer">
-        <Accordion className="customAccordion">
-          <Typography>
-            <div className="customAnswer">
-              <ul>
-                <li>
-                  <b> معرفی صبا جهاد:</b>
-                  شرکت کارگزاری صباجهاد در تاریخ 1383/06/08 و با شماره 229185 در
-                  اداره ثبت شرکت‌ها و مؤسسات غیرتجاری تهران به ثبت رسید و از اول
-                  مهرماه همان سال با افتتاح تالار بورس کالای کشاورزی، فعالیت خود
-                  را آغاز نمود. صباجهاد با تکیه بر سرمایه انسانی متخصص و با هدف
-                  ارائه راهکارهای تخصصی مالی در صنعت واسطه گری مالی و بنگاه داری
-                  (بازار سرمایه، بازار پول و هلدینگ ها) فعالیت می کند.
-                </li>
-              </ul>
-            </div>
-          </Typography>
-        </Accordion>
-      </div>
-    </div>
-  );
+	return (
+		<div className="page" style={{ textAlign: "center" }}>
+			<div className="fullImage about"></div>
+			<div>
+				<div className="split big centered">
+					<h2>معرفی کارگزاری صبا جهاد</h2>
+					<p>
+						شرکت کارگزاری صباجهاد در تاریخ 1383/06/08 و با شماره 229185 در اداره
+						ثبت شرکت‌ها و مؤسسات غیرتجاری تهران به ثبت رسید و از اول مهرماه همان
+						سال با افتتاح تالار بورس کالای کشاورزی، فعالیت خود را آغاز نمود.{" "}
+					</p>
+				</div>
+			</div>
+
+			<h2 className="small">اعضای هیئت مدیره</h2>
+			<div className="personContainer">
+				<div className="personCard">
+					<div className="image mahnush"></div>
+					<div className="title">خانم مهنوش کریم</div>
+					<div className="role">عضو هیات‌ مدیره</div>
+				</div>
+				<div className="personCard">
+					<div className="image alireza"></div>
+					<div className="title">آقای علیرضا مرادی</div>
+					<div className="role">مدیرعامل و نایب رئیس هیات مدیره</div>
+				</div>
+				<div className="personCard">
+					<div className="image ali"></div>
+					<div className="title">آقای علی صلاحی نژاد</div>
+					<div className="role">رئیس هیات مدیره</div>
+				</div>
+				<div className="personCard">
+					<div className="image mohamad"></div>
+					<div className="title">آقای محمد طهرانی</div>
+					<div className="role">مدیر امور اداری</div>
+				</div>
+				<div className="personCard">
+					<div className="image abolfazl"></div>
+					<div className="title">آقای ابوالفضل ابراهیم زاده</div>
+					<div className="role">مدیر معاملات اوراق بهادار</div>
+				</div>
+				<div className="personCard">
+					<div className="image milad"></div>
+					<div className="title">آقای میلاد کلهری</div>
+					<div className="role">مدیر معاملات بورس کالا </div>
+				</div>
+				<div className="personCard">
+					<div className="image noperson"></div>
+					<div className="title">آقای مسعود مسافرچی</div>
+					<div className="role">مدیر شعب</div>
+				</div>
+				<div className="personCard">
+					<div className="image salman"></div>
+					<div className="title">آقای سلمان سلمانی</div>
+					<div className="role">مدیرمالی</div>
+				</div>
+				<div className="personCard">
+					<div className="image vahid"></div>
+					<div className="title">آقای وحید قاسمی</div>
+					<div className="role">مدیر حراست</div>
+				</div>
+			</div>
+
+			<div>
+				<div className="split big centered">
+					<h2 className="small">پیام مدیر عامل</h2>
+					<p>
+						<br></br>
+						<b>آقای علی صلاحی نژاد</b>
+						<br></br>
+						<br></br>
+						توسعه اقتصادی کشور در گرو رونق بازار سرمایه است. در این راستا در
+						کارگزاری بورس بیمه ایران برآن شدیم که با اعتلای تخصص، توسعه فناوری و
+						همت روزافزون همکاران با بهره گیری از بیش از ۲۶ سال تجربه ارزشمند،
+						خدمات خود را بهبود بخشیده تا نقشی در رونق بازار سرمایه و تعالی
+						اقتصادی کشور داشته باشیم.
+					</p>
+				</div>
+			</div>
+			<div className="fullImage about2"></div>
+			<h2 className="small">چارت سازمانی</h2>
+			<div className="fullImage clong long chart"></div>
+			<div className="viewCardContainer">
+				<div className="viewCard">
+					<div className="top">
+						<div className="role">چشم‌انداز</div>
+						<div className="image view1"></div>
+					</div>
+					مرجع پیشرو و سرآمد در صنعت خدمات مالی و سرمایه گذاری با ارائه
+					راهکارهای تخصصی و روزآمد مالی در راستای بهینه سازی تصمیمات مالی با افق
+					سال 1400
+				</div>
+				<div className="viewCard">
+					<div className="top">
+						<div className="role">ماموریت</div>
+						<div className="image view2"></div>
+					</div>
+					شرکتی است دانشی و روزآمد که با رویکرد تبلور دانش مالی در ارزش‌آفرینی،
+					با تکیه بر سرمایه انسانی متخصص و با هدف ارائه راهکارهای تخصصی مالی در
+					صنعت واسطه گری مالی و بنگاه داری (بازار سرمایه، بازار پول و هلدینگ ها)
+					فعالیت می کند.
+				</div>
+			</div>
+
+			<div className="compartmentContainer" style={{ textAlign: "right" }}>
+				<div className="compartment right">
+					<div className="data">
+						<h2>افتخارات</h2>
+						<p>
+							اطمینان شهروندان و هدف‌گذاری اصولی منجر به کسب رتبه‌های برتر بازار
+							سرمایه توسط کارگزاری بورس بیمه ایران شده است:
+							<ul>
+								<li>جایگاه نخست در بین کارگزاری‌های دولتی و نهادی</li>
+								<li>رتبه هشتم برترین کارگزاری‌های کشور در بورس اوراق بهادار</li>
+							</ul>
+						</p>
+					</div>
+					<div className="image radius about3"></div>
+				</div>
+			</div>
+
+			<div className="compartmentContainer" style={{ textAlign: "right" }}>
+				<div className="compartment left">
+					<div className="image radius about4"></div>
+					<div className="data">
+						<h2>مسئولیت اجتماعی</h2>
+						<p>
+							میزان تعهد هر نهاد به برنامه‌هایش را می‌توان درگستره تاثیرگذاری‌اش
+							در جامعه جست‌و‌جو کرد؛<br></br> کارگزاری بورس بیمه ایران به عنوان
+							نهادی اقتصادی – اجتماعی تلاش کرده است تا با حفظ ارتباط خود با
+							نهادهای اجتماعی در راستای کمک به بهبود اشتغال، درآمدزایی و
+							خوداتکایی شهروندان حرکت کند. طی سال‌های اخیر همکاری با انجمن‌های
+							مردم نهاد از جمله محک، رعد الغدیر و ... در بخش‌های مختلف منجر به
+							اشتغالزایی ده‌ها نفری و رفع نیازهای ضروری خانواده‌های نیازمند شده
+							است. این کارگزاری آماده همکاری با نهادهای مدنی مختلف برای کمک به
+							بهبود رفاه اجتماعی است.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Presentation;
